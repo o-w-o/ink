b9688264f3d1efeab891241fd9efb58f6c10ef9d
o-w-o <symbols@dingtalk.com>
2020-01-14 22:00:51 +0800

    feat($smart-commits): :tada: comment
    INK-22 #comment 测试评论内容

---
+++ script/test.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+console.log("[TEST] jira.[Smart Commits]");
+
+/** 
+ * When you manage your project's repositories in Bitbucket or GitHub, or use Fisheye to browse and search your repositories, 
+ * you can process your Jira Software issues using special commands, called Smart Commits, in your commit messages.
+ * You can:
+ * 
+ *  - comment on issues
+ *  - record time tracking information against issues
+ *  - transition issues to any status defined in the Jira Software project's workflow. 
+ * 
+ * There are other actions available if you use Crucible for software reviews. See Using Smart Commits in the Crucible documentation.
+ * A Smart Commit command must not span more than one line (i.e. you cannot use carriage returns in the command), 
+ * but you can add multiple commands to the same line. See this example below.
+ * 
+ * # Smart Commit commands
+ * The basic syntax for a Smart Commit message is:
+ * 
+ *  - <ignored text> <ISSUE_KEY> <ignored text> #<COMMAND> <optional COMMAND_ARGUMENTS>
+ * 
+ * Any text between the issue key and the command is ignored.
+ * 
+ * There are three commands you can use in your Smart Commit messages:
+ *  - comment
+ *  - time
+ *  - transition
+ */
